8d02a2a600a06899e94c98b5e4dae3e92451fd1a
Krysleigh <148019679+Krysleigh@users.noreply.github.com>
2023-12-18 15:59:30 -0800

    commit

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,4 +1,13 @@
 import os
 import csv
 
-csvpath= os.path.join ('..', 'Resources', 'budget_data.csv')
+budget_data= os.path.join("Resources", "budget_data.csv")
+	total_month= 0
+	total_profitloss= 0
+	value= 0
+	change= 0
+	dates= []
+	profits= []
+
+with open("budget_data") as csvfile:
+	csvreader=csv.reader(csvfile)
