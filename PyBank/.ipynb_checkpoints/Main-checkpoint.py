7ebf45eb1623611aeb3f0421ac944e27d6bfe9b6
Krysleigh <148019679+Krysleigh@users.noreply.github.com>
2024-02-01 21:12:45 -0800

    update

---
+++ PyBank/.ipynb_checkpoints/Main-checkpoint.py
@@ -0,0 +1,67 @@
+import os
+import csv
+
+budget_data= os.path.join("Resources","budget_data.csv")
+total_month= 0
+total_profitloss= 0
+value= 0
+net_change= 0
+dates= []
+profits= []
+
+# Variables to track greatest increase and decrease
+greatest_increase= 0
+greatest_decrease= 0
+gi_date= ""
+gd_date= ""
+
+with open(budget_data) as csv_file:
+	csv_reader=csv.reader(csv_file, delimiter=",")
+
+	csv_header= next(csv_reader)
+
+
+	for row in csv_reader:
+		date= row[0]
+		total_month+= 1
+		profitloss = int(row[1])
+		total_profitloss+= profitloss
+		
+		if total_month>1:
+			net_change+= profitloss - profits[-1]
+		
+		dates.append(date)
+		profits.append(profitloss)
+		
+		# Check for greatest increase
+		if profitloss > greatest_increase:
+			greatest_increase= profitloss
+			greatest_increase_date= date
+		# Check for greatest decrease
+		if profitloss < greatest_decrease:
+			greatest_decrease= profitloss
+			greatest_decrease_date= date
+
+# Calculate average change
+avg_profit_change= net_change / (total_month -1)
+
+print(f"Financial Analysis")
+print(f"------------------------------------------")
+print(f"Total Months: {total_month}")
+print(f"Total: (${total_profitloss})")
+print(f"Average Change {avg_profit_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
+
+# writing multiple lines in a Text File
+import os
+analysis_output = os.path.join('analysis', 'PyBankAnaysis.txt')
+
+with open ('analysis_output', 'w') as f:
+	f.write("Financial Analysis\n")
+	f.write("--------------------------------------\n")
+	f.write(f"Total Months: {total_month}\n")
+	f.write(f"Total: (${total_profitloss})\n")
+	f.write(f"Average Change {avg_profit_change:.2f}\n")
+	f.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n")
+	f.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
---
+++ PyPoll/Main.py
@@ -0,0 +1,85 @@
+import os
+import csv
+
+csv_file_path = "Resources/election_data.csv"
+
+total_votes = 0
+ballot_id = []
+county = []
+candidate = []
+
+# Counting total votes and setting candidate votes to zero
+charles = 0
+diana = 0
+raymon = 0
+
+with open(csv_file_path) as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=",")
+
+    # Count total votes
+	total_votes = sum(1 for row in csv_reader)
+
+# Resetting the file pointer to the beginning of the file
+with open(csv_file_path) as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=",")
+
+	for row in csv_reader:
+        # votes for candidates
+		if row[2] == "Charles Casper Stockham":
+			charles += 1
+		elif row[2] == "Diana DeGette":
+			diana += 1
+		else:
+			raymon += 1
+		ballot_id.append(row[0])  # Assuming ballot ID is in the first column
+
+# percentages of votes per candidate
+percent_charles = round(charles / len(ballot_id) * 100, 3)
+percent_diana = round(diana / len(ballot_id) * 100, 3)
+percent_raymon = round(raymon / len(ballot_id) * 100, 3)
+
+# printing election results
+print("Election Results")
+# printing separating line
+print("---------------------------")
+# printing total number of votes
+print("Total Votes:", total_votes)
+# printing separating line
+print("---------------------------")
+# printing the votes for each candidate
+print("Charles Casper Stockham: " + str(percent_charles) + "%  (" + str(charles) + ")")
+print("Diana DeGette: " + str(percent_diana) + "%  (" + str(diana) + ")")
+print("Raymon Anthony Doane: " + str(percent_raymon) + "%  (" + str(raymon) + ")")
+# printing separating line
+print("---------------------------")
+# printing the winning candidate
+if charles > diana and charles > raymon:
+    print("Winner: Charles Casper Stockham")
+elif diana > charles and diana > raymon:
+    print("Winner: Diana DeGette")
+else:
+    print("Winner: Raymon Anthony Doane")
+print("---------------------------")
+# writing multiple lines in a Text File
+
+import os
+
+analysis_output = os.path.join('analysis', 'PypollAnaysis.txt')
+
+with open(analysis_output, 'w') as f:
+	f.write("Election Results\n")
+	f.write("--------------------------------------\n")
+	f.write(f"Total Votes: {total_votes}\n")
+	f.write("--------------------------------------\n")
+	f.write(f"Charles Casper Stockham: {percent_charles}%  ({charles})\n")
+	f.write(f"Diana DeGette: {percent_diana}%  ({diana})\n")
+	f.write(f"Raymon Anthony Doane: {percent_raymon}%  ({raymon})\n")
+	f.write("--------------------------------------\n")
+	if charles > diana and charles > raymon:
+		f.write("Winner: Charles Casper Stockham\n")
+	elif diana > charles and diana > raymon:
+		f.write("Winner: Diana DeGette\n")
+	else:
+		f.write("Winner: Raymon Anthony Doane\n")
+	f.write("--------------------------------------\n")
+
